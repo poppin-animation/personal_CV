86d60b52bbd6917ee80e115b14d3cf3a535221c5
Florencia S. Sueiro <62158341+poppin-animation@users.noreply.github.com>
2022-04-06 15:51:02 -0300

    Inicio
    Primer Commit

---
+++ mensaje.js
@@ -0,0 +1,16 @@
+
+function mensaje(){
+    alert("Hola Mundo");
+}
+
+function menparr(){
+    alert("Acabamos de modificar la alerta para el parrafo");
+}
+
+
+//comentario de 1 linea 
+
+/**comentario de 
+ * varias lineas 
+ * en js
+ */
